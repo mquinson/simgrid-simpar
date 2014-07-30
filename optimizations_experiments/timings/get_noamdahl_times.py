261046079f62127a4f9dcda89ea2f4b6eac36e60
etortilopez <ruben-ezequiel.torti-lopez@inria.fr>
2014-07-30 17:15:15 +0200

    ++report, upgrade scripts, update journal

--- optimizations_experiments/timings/get_noamdahl_times.py
+++ optimizations_experiments/timings/get_noamdahl_times.py
@@ -1,9 +1,9 @@
 DIR = "log"
-SIZES = ['1000', '3000', '5000', '10000']
+SIZES = ['1000',  '10000']
 MODES = ['constant', 'precise']
 THREADS = ['1', '2', '4', '8', '16', '24']
-output_file = "total_times_noamdahl.log"
-
+output_file = "total_times_noamdahl2.log"
+input_seq = ['', '2']
 
 def parse_files():
     f = open(output_file, "w")
@@ -12,14 +12,20 @@ def parse_files():
         temp_line = "{}\t".format(size)
         for mode in MODES:
             for thread in THREADS:
-                file = open("{}/chord_{}_threads{}_{}.log".format(DIR, size, thread, mode), "r")
-                line = file.read().splitlines()[-1]
-                if line:
-                    usrtime = float((line.split(":")[2]).split()[0])
-                    systime = float((line.split(":")[3]).split()[0])
-                    tottime = usrtime + systime
-                # tuples containing (sequential,parallel) time
-                    temp_line += "{}\t".format(tottime)
+                sum_l = 0.
+                leng = len(input_seq)
+                for seq in input_seq:
+                    file = open("{}/chord{}_{}_threads{}_{}.log".format(DIR, seq, size, thread, mode), "r")
+                    line = file.read().splitlines()[-1]
+                    if line:
+                        usrtime = float((line.split(":")[2]).split()[0])
+                        systime = float((line.split(":")[3]).split()[0])
+                        sum_l = usrtime + systime
+                    # tuples containing (sequential,parallel) time
+                    else:
+                        leng -= 1
+                if leng!=0:
+                    temp_line += "{}\t".format(sum_l / float(leng))
                 else:
                     temp_line += "?\t?\t"
         f.write(temp_line + "\n")
