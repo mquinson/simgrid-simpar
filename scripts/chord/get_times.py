a478c722eed6c599908589c1f31f4a3f2e0fe113
etortilopez <ruben-ezequiel.torti-lopez@inria.fr>
2014-09-08 10:59:36 +0200

    now get_times generates a .csv

--- scripts/chord/get_times.py
+++ scripts/chord/get_times.py
@@ -1,16 +1,21 @@
-DIR = "log"
-MODES = ['constant', 'precise']
-SIZES = ['1000', '3000', '10000', '30000', '100000'] #, '100000'] #, '300000']
+DIR = "log"  # The dir where to gather the logs
+MODES = ['constant', 'precise']  # The modes you want to analyse
+SIZES = ['1000', '3000', '10000', '30000', '100000']  #, '100000'] #, '300000']
 THREADS = ['2', '4', '8', '16', '24']
-output_file = "total_times_noamdahl2.log"
-input_seq = ['']
+output_file = "total_times.csv"  # Modify this name to a proper one.
+
+input_seq = ['']  # If you make several test of the same experiment, you can
+                  # name the log files with a
+                  # prefix ('1_chord..., 2_chord...') and then put the prefixes
+                  # you used here. The script will average the corresponding
+                  # values for you.
 
 
 def parse_files():
     f = open(output_file, "w")
-    f.write("#Non bencharked timings\n#constant first, precise last\n")
+    # f.write("#Non bencharked timings\n#constant first, precise last\n")
     for size in SIZES:
-        temp_line = "{}\t".format(size)
+        temp_line = "{}".format(size)
         for mode in MODES:
             for thread in THREADS:
                 sum_l = 0.
@@ -26,9 +31,9 @@ def parse_files():
                     else:
                         leng -= 1
                 if leng != 0:
-                    temp_line += "{}\t".format(sum_l / float(leng))
+                    temp_line += ",{}".format(sum_l / float(leng))
                 else:
-                    temp_line += "?\t?\t"
+                    temp_line += ",?"
         f.write(temp_line + "\n")
     f.close()
 if __name__ == "__main__":
