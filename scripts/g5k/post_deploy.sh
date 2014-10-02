b9a57a05f89238b558a2f9f10db99b0deb43bad2
etortilopez <ruben-ezequiel.torti-lopez@inria.fr>
2014-10-02 11:51:45 +0200

    script for post-deployment in g5k added, in case its useful for someone

---
+++ scripts/g5k/post_deploy.sh
@@ -0,0 +1,23 @@
+# Before running this, first do ssh-keygen and add the key to authorized keys of rennes.
+set -e
+# First set proxys (to allow pull from SimGrid repository)
+export http_proxy=http://proxy.rennes.grid5000.fr:3128/ && export https_proxy=https://proxy.rennes.grid5000.fr:3128/
+scp rtortilopez@rennes.grid5000.fr:~/ssh_files/* .ssh/.
+cd SimGrid/
+
+if [ -z "$1" ]
+then
+    SG_COMMIT=$1
+    git checkout $SG_COMMIT
+fi
+
+cmake -Denable_compile_optimizations=ON -Denable_supernovae=OFF -Denable_compile_warnings=OFF -Denable_debug=OFF -Denable_gtnets=OFF -Denable_jedule=OFF -Denable_latency_bound_tracking=OFF -Denable_lua=OFF -Denable_model-checking=OFF -Denable_smpi=OFF -Denable_tracing=OFF -Denable_documentation=OFF .
+make install
+cd examples/msg/chord/
+# Test scripts
+scp rtortilopez@rennes.grid5000.fr:~/testall.sh .
+scp rtortilopez@rennes.grid5000.fr:~/generate.py .
+scp rtortilopez@rennes.grid5000.fr:~/testall_sr.sh .
+# Copy some platform and deployment files
+scp rtortilopez@rennes.grid5000.fr:~/platforms/*.xml .
+scp rtortilopez@rennes.grid5000.fr:~/deployments/*.xml .
